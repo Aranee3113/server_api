b270459679b0c6b8f36e4dcdddaab9df8b78b1b9
sarocha39 <149657829+sarocha39@users.noreply.github.com>
2025-08-28 13:59:26 +0700

    แก้ตรงอนุมัติ

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,12 +1,16 @@
+// post.routes.ts
 import { Elysia } from "elysia";
 import { post_controller } from "../controllers/post.controller";
 
 export const postRoutes = new Elysia({ prefix: "/post" })
-  .get("/", post_controller.getAllposts)                // GET /post
-  .get("/:id", post_controller.getpostById)             // GET /post/:id
-  .post("/", post_controller.createpost)                // POST /post
-  .put("/:id", post_controller.updatepostById)          // PUT /post/:id
-  .delete("/:id", post_controller.deletepostById)      // DELETE /post/:id
-  .get("/active", post_controller.getPostIsActive)        // GET /post is active
-  .put("/active/:id", post_controller.updateStatusActive)
-  .get("/:id/full", post_controller.getPostWithComments) // GET /post/:id/full
+  // ---- Active routes มาก่อน ----
+  .get("/active", post_controller.getPostIsActive)          // GET /post/active
+  .put("/active/:id", post_controller.updateStatusActive)   // PUT /post/active/:id
+
+  // ---- routes อื่น ๆ ----
+  .get("/", post_controller.getAllposts)                    // GET /post
+  // .get("/:id/full", post_controller.getPostWithComments)    // GET /post/:id/full
+  .post("/", post_controller.createpost)                    // POST /post
+  .put("/:id", post_controller.updatepostById)              // PUT /post/:id
+  .delete("/:id", post_controller.deletepostById)           // DELETE /post/:id
+  .get("/:id", post_controller.getpostById);                // GET /post/:id
