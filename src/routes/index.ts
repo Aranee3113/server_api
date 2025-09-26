7fd03fd354c73eabf5f5404c47f34cc8b07d5b76
Aranee3113 <150328224+Aranee3113@users.noreply.github.com>
2025-09-26 19:29:25 +0700

    ลบข้อมูลผ้าทอ กับความนิยมออก

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,18 @@
 import { Elysia } from "elysia";
 import { user_route } from "./user.route";
 import { auth_route } from "./guard.route";
-import { productRoutes } from "./product.route";
 import { postRoutes } from "./post.route";
 import { commentRoutes } from "./comment.route";
 import { ratingRoute } from "./rating.route";
-import { textileRatingRoute } from "./textile-rating.route";
 
 const r = new Elysia();
 
 r.group("/api", (app) =>
   app.use(user_route)
      .use(auth_route)
-     .use(productRoutes)
      .use(postRoutes)
      .use(commentRoutes)
      .use(ratingRoute)
-     .use(textileRatingRoute)
 );
 
 export default r;
